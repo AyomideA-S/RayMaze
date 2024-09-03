1e27d83df4d2ab524c78671df759e6d877c93f6f
AyomideA-S <midesuperbest@gmail.com>
2024-09-03 17:33:24 +0100

    Draw line

--- src/raycaster.c
+++ src/raycaster.c
@@ -6,5 +6,5 @@ void render_scene(SDL_Renderer *renderer)
     /* Set the colour to draw */
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
     /* Draw the vertical stripe for the current column */
-    SDL_RenderDrawPoint(renderer, 100, 100);
+    SDL_RenderDrawLine(renderer, 100, 0, 100, SCREEN_HEIGHT);
 }
