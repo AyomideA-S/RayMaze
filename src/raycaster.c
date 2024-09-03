e80c298f0d4ffff1a9a4319fe87f5e61eaed08cf
AyomideA-S <midesuperbest@gmail.com>
2024-09-03 17:48:49 +0100

    Draw loop

--- src/raycaster.c
+++ src/raycaster.c
@@ -1,10 +1,18 @@
 #include "main.h"
 #include "raycaster.h"
 
+/**
+ * render_scene - Render the scene using raycasting techniques
+ *
+ * @renderer: The renderer to use
+ */
 void render_scene(SDL_Renderer *renderer)
 {
-    /* Set the colour to draw */
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-    /* Draw the vertical stripe for the current column */
-    SDL_RenderDrawLine(renderer, 100, 0, 100, SCREEN_HEIGHT);
+	for (int x = 0; x < SCREEN_WIDTH; x++)
+	{
+		/* Set the colour to draw */
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+		/* Draw the vertical stripe for the current column */
+		SDL_RenderDrawLine(renderer, x, 0, x, SCREEN_HEIGHT);
+	}
 }
